caebba5e7c686de8e0bd09ad77c98d5a5c1e3e49
Augusto <augstarachrafael@gmail.com>
2020-01-16 20:23:27 -0300

    coisas iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { SecaoComponent } from './secao/secao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    SecaoComponent
   ],
   imports: [
     BrowserModule
