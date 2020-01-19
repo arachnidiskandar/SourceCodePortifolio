339e8d1c8cc16ef2527b2f88f3df76932ab94e0a
Augusto <augstarachrafael@gmail.com>
2020-01-19 02:17:42 -0300

    Navbar animação

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('mudarNavBar', [
+      state('inicial', style({
+        color: 'white',
+        backgroundColor: 'transparent'
+      })),
+      state('final', style({
+        color: ' #4168a3',
+        backgroundColor: 'white'
+      })),
+      transition('inicial=>final', animate('200ms')),
+      transition('final=>inicial', animate('200ms'))
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
-
+  estado = 'inicial';
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+    onScroll(event) {
+      window.pageYOffset > 560 ? this.estado = 'final' : this.estado = 'inicial';
+  }
   constructor() { }
 
   ngOnInit() {
   }
-  
+  mudarEstado() {
+    
+  }
 }
