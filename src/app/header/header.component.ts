a51c376223c9498554a8cf6a0f985416e6f37f33
Augusto <augstarachrafael@gmail.com>
2020-01-19 20:31:20 -0300

    Navbar cheia de gambi

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,14 +16,26 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         color: ' #4168a3',
         backgroundColor: 'white'
       })),
-      transition('inicial=>final', animate('200ms')),
-      transition('final=>inicial', animate('200ms'))
+      transition('inicial=>final', animate('2ms')),
+      transition('final=>inicial', animate('2ms'))
     ])
+    // trigger('mostrarMenu', [
+    //   state('inicial', style({
+    //     top: '0vh'
+    //   })),
+    //   state('final', style({
+    //     top: '8vh'
+    //   })),
+    //   transition('inicial=>final', animate('2000ms')),
+    //   transition('final=>inicial', animate('2000ms'))
+    // ])
   ]
 })
 export class HeaderComponent implements OnInit {
   estado = 'inicial';
-  @HostListener('window:scroll', ['$event']) // for window scroll events
+  menu = false;
+
+  @HostListener('window:scroll', ['$event'])
     onScroll(event) {
       window.pageYOffset > 560 ? this.estado = 'final' : this.estado = 'inicial';
   }
@@ -31,7 +43,23 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
   }
-  mudarEstado() {
-    
+  mudarEstilo() {
+    if (this.estado === 'inicial') {
+      return {
+        color: 'white',
+        borderBottom: 'white'
+      };
+    } else {
+      return {
+        color: '#4168a3',
+        borderBottom: '#4168a3'
+      };
+    }
+  }
+  ativarMenu() {
+    if (window.pageYOffset < 560) {
+      this.estado = 'final';
+    }
+    this.menu ? this.menu = false : this.menu = true;
   }
 }
